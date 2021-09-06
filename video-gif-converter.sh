03d570a316bf01cc83633f5c2fb6debd6aa6cf6a
Ahmed Yasser <ahmedyasser4222@gmail.com>
2021-09-07 00:55:51 +0200

    Finished script

--- video-gif-converter.sh
+++ video-gif-converter.sh
@@ -3,22 +3,31 @@
 #Videos path
 if [[ "$1" == "" ]]
 then
-    echo "Enter the absolute path of the videos' directory as written(Example: /home/user/Desktop/Videos\ to\ be\ converted/)"
-    read -re vdir;
+    read -re -p "Enter the absolute path of the videos' directory as written (Example: /home/user/Desktop/Videos\ to\ be\ converted/): " vdir
 else
+    #Set path
     vdir="$1";
 fi
 
+#Change directory to videos path
 cd "$vdir"
 
+#Handle spaces in names
+#Preserve IFS variable
+IFS_BACKUP=$IFS
+IFS=$(echo -en "\n\b")
+#IFS=$(echo -en "\n\r")
+
 #Loop over videos
-for fvid in $(ls)
+for fvid in *.mp4
 do
-    cat "$fvid"
-    # ffmpeg -i input.mp4 -filter_complex "crop=iw-900" input_cropped.mp4
-    # ffmpeg -i input_cropped.mp4 -vf scale=210x210:flags=lanczos -c:v libx264 -preset slow -crf 21 scaled_input.mp4
-    # rm input_cropped.mp4
-    # ffmpeg -y -i scaled_input.mp4 -vf fps=10,scale=210:-1:flags=lanczos,palettegen palette.png
-    # ffmpeg -i scaled_input.mp4 -i palette.png -filter_complex "fps=10,scale=210:-1:flags=lanczos[x];[x][1:v]paletteuse" output.gif
-    # rm scaled_input.mp4 palette.png
+    ffmpeg -i "$fvid" -filter_complex "crop=iw-900" "${fvid}_cropped.mp4"
+    ffmpeg -i "${fvid}_cropped.mp4" -vf scale=210x210:flags=lanczos -c:v libx264 -preset slow -crf 21 "${fvid}_scaled.mp4"
+    rm "${fvid}_cropped.mp4"
+    ffmpeg -y -i "${fvid}_scaled.mp4" -vf fps=10,scale=210:-1:flags=lanczos,palettegen "${fvid}_palette.png"
+    ffmpeg -i "${fvid}_scaled.mp4" -i "${fvid}_palette.png" -filter_complex "fps=10,scale=210:-1:flags=lanczos[x];[x][1:v]paletteuse" "${fvid}.gif"
+    rm "${fvid}_scaled.mp4" "${fvid}_palette.png"
 done
+
+#Preserve IFS variable
+IFS=$IFS_BACKUP
